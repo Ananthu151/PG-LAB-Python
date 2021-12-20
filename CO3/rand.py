1436fda36df3ff798b6c610dc827ef62aadaaa38
Ananthu151 <93853939+Ananthu151@users.noreply.github.com>
2021-12-20 20:29:46 +0530

    Update rand.py

--- CO3/rand.py
+++ CO3/rand.py
@@ -1,1 +1,8 @@
 import random
+random.seed(10)
+print(random.random())
+print(random.getstate())
+mylist = ["Ananthu", "Manu", "Kumar"]
+print(random.choice(mylist))
+print(random.uniform(654, 64))
+print(random.triangular(54, 70, 364))
